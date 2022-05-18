ac5b4f65a7c624bbd529e967b0254d276abaac47
Prohitto <thelollertroller@gmail.com>
2022-05-18 17:49:05 +0200

    Update TCPClient.java

--- TCP_Date_Gruppo3/src/TCPClient.java
+++ TCP_Date_Gruppo3/src/TCPClient.java
@@ -43,18 +43,12 @@ public class TCPClient {
 	}
 
 	public static void main (String args[]){
-		String server;
-		int port;
+		String server = "127.0.0.1";
+		int port = 13;
 		String daytime;
 		TCPClient client ;
-		if (args.length != 3) {
-			server = "127.0.0.1"; //localhost
-			port = 13; //porta TCP standard del servizio daytime
-		}
-		else{
-			server = args[0];
-			port = Integer.parseInt(args[1]);
-		}
+		
+		
 		try {
 			client = new TCPClient(server, port);
 			daytime = client.getDaytime();
